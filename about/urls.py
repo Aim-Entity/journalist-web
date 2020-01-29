df466ad298e934c6c8fa9898f735359b06ccf74c
Bilal <bilaluddin474@Gmail.com>
2020-01-29 14:54:56 +0000

    Setup about

---
+++ about/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+name_space = "about"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
--- adventure/settings.py
+++ adventure/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "landing_page",
     "join",
-    "about",
+    "about"
 ]
 
 MIDDLEWARE = [
